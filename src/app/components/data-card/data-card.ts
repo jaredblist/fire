d91cbdddb0d844f120dc99f1e596cc725f8ac41f
Jaredblist <jaredbeaudrot@gmail.com>
2026-07-12 15:36:13 -0400

    basic setup

---
+++ src/app/components/data-card/data-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-card',
+  imports: [],
+  templateUrl: './data-card.html',
+  styleUrl: './data-card.css',
+})
+export class DataCard {
+
+}
